04cd88fe4dffe629fdf7809bb633ee11186b3eb9
Sam <samnap+github@gmail.com>
2013-11-14 22:37:17 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blissrun3::Application.config.secret_token = 'd4912ec7044b0902c299b69b9e90636448cf5fdbd42e117286f136b5a4bde9c82abc608f1ec6390d7ad5a1e431f063854de73a01118b8a8380e7e53512e18803'
