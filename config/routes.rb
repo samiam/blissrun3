b4f76fe7f0bd01428935b4afa734253dcc5f8248
Sam <samnap+github@gmail.com>
2013-11-17 12:15:56 -0500

    Add routes and initializer

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,12 @@ Blissrun3::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
+   # Root goes 'home' and other routes exist corresponding action
+   root                :controller => "home",    :action => "home"
+   match  "/docs/:doc", :controller => "docs", :action => "show",
+                              :requirements => { :doc => /[\w]+/ }
+   match  ":action", :controller => "home"
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
