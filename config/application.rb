8f383db77be1051748a5b166b0814eb54791f7c6
Sam <samnap+github@gmail.com>
2013-11-20 22:28:37 -0500

    Try getting app to work in production environment

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module Blissrun3
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['default.css']
   end
 end
